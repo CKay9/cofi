4dbc619158b02732452359ebcd1ca3824c588f1c
CKay9 <chris.kvesa@gmail.com>
2025-03-22 00:12:59 +0100

    fix(ui): cursor made invisible

--- src/modules/terminal.zig
+++ src/modules/terminal.zig
@@ -4,11 +4,17 @@ const termios = @cImport({
     @cInclude("termios.h");
 });
 
+const HIDE_CURSOR = "\x1b[?25l";
+const SHOW_CURSOR = "\x1b[?25h";
+
 pub fn enableRawMode() !void {
     var raw: termios.termios = undefined;
     _ = termios.tcgetattr(0, &raw);
     raw.c_lflag &= ~@as(c_uint, termios.ECHO | termios.ICANON);
     _ = termios.tcsetattr(0, termios.TCSAFLUSH, &raw);
+
+    const stdout = std.io.getStdOut().writer();
+    try stdout.writeAll(HIDE_CURSOR);
 }
 
 pub fn disableRawMode() void {
@@ -16,4 +22,7 @@ pub fn disableRawMode() void {
     _ = termios.tcgetattr(0, &raw);
     raw.c_lflag |= termios.ECHO | termios.ICANON;
     _ = termios.tcsetattr(0, termios.TCSAFLUSH, &raw);
+
+    const stdout = std.io.getStdOut().writer();
+    stdout.writeAll(SHOW_CURSOR) catch {};
 }
